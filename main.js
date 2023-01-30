93d278437a4a4c253bcf302d040d9b9fa6049df1
JosselynD88 <123917107+JosselynD88@users.noreply.github.com>
2023-01-29 21:45:28 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// Tempeture in Kelvin degrees  
+const Kelvin = 0
+console.log(Kelvin);
+// Converting kelvin into celsius 
+const Celsius = Kelvin - 273;
+console.log (Celsius);
+// Calculating Fahrenheit
+let Fahrenheit = Celsius * (9/5) + 32;
+console.log(Fahrenheit);
+// Rounding the Tempeture
+console.log(Math.floor(Fahrenheit));
+// String Temperature
+console.log (`The temperature is ${Fahrenheit} degrees Fahrenheit.` );
+// Extra practice
+const Newton = Celsius * (33/100)
+console.log(Newton);
+console.log(`The temperature is ${(Match.floor(Newton))}.`);
